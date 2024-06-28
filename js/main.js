372afe2f0c4edd8f0e75c846dd45256d16948a24
Kamil <kamil.szewczyk.nsz@gmail.com>
2024-06-28 18:59:37 +0200

    Finished game

--- js/main.js
+++ js/main.js
@@ -1,54 +1,53 @@
 'use strict';
 
+const message = document.querySelector('.message');
 
+const yourScore = document.querySelector('.score');
 
-// document.querySelector('.guess').value = 20;
+const number = document.querySelector('.number');
 
+const guessInput = document.querySelector('.guess');
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 
 document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value);
+    const guess = Number(guessInput.value);
     console.log(guess, typeof guess);
     
     if(!guess) {
-        document.querySelector('.message').textContent = 'Podaj liczbę!';
+        message.textContent = 'Podaj liczbę!';
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Dobrze!';
-        document.querySelector('.number').textContent = secretNumber;
+        message.textContent = 'Dobrze!';
+        number.textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         // document.querySelector('.number').style.width = '30rem'
-    } else if( guess > secretNumber) {
-        if(score > 1) {
-            document.querySelector('.message').textContent = 'Za dużo!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'Przegrałeś!';
-            document.querySelector('.score').textContent = 0;
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
         }
-        
-    } else if( guess < secretNumber) {
+    } else if(guess !== secretNumber) {
         if(score > 1) {
-            document.querySelector('.message').textContent = 'Za mało!';
+            message.textContent = guess > secretNumber ? 'Za dużo!' : 'Za mało!';
             score--;
-            document.querySelector('.score').textContent = score;
+            yourScore.textContent = score;
         } else {
-            document.querySelector('.message').textContent = 'Przegrałeś!';
-            document.querySelector('.score').textContent = 0;
+            message.textContent = 'Przegrałeś!';
+            yourScore.textContent = 0;
         }
-    }
+
+    } 
 })
 
 
 document.querySelector('.again').addEventListener('click', function() {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.message').textContent = 'Zacznij zgadywać!';
-    document.querySelector('.score').textContent = score;
-    document.querySelector('.number').textContent = '?';
-    document.querySelector('.guess').value = '';
+    message.textContent = 'Zacznij zgadywać!';
+    yourScore.textContent = score;
+    number.textContent = '?';
+    guessInput.value = '';
     document.querySelector('body').style.backgroundColor = '#222';
 })
